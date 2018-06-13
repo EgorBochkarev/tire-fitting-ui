bd3b9b1e775519452d507a9c30bb6b1d7a4970c4
Vlad <templerock@mail.ru>
2018-06-13 18:26:11 +0300

    reloaded (new) view of registration with regax

--- app/view/registration/Registration.js
+++ app/view/registration/Registration.js
@@ -1,4 +1,13 @@
-Ext.define('Tf.view.registration.Registration', {
+var passNumberVType = {
+    passNumber: function(val, field){
+        var passNumberRegex = /^\d/;
+        return passNumberRegex.test(val);
+    },
+    passNumberText: 'Must be a Number',
+    passNumberMask: /[\d]/
+};
+Ext.apply(Ext.form.field.VTypes, passNumberVType);
+Ext.define('Tf.view.registration.Registration',{
     extend: 'Ext.panel.Panel',
     controller: 'main',
     id: 'registrationWindow',
@@ -13,115 +22,237 @@ Ext.define('Tf.view.registration.Registration', {
         }
     },
     layout: 'center',
-    bodyPadding: '50',
-    items: [{
-        xtype: 'panel',
-        bodyPadding: '10 10',
-        title: 'Registration form',
-        scrollable: true,
-        width: 300,
-        layout: {
-            type: 'vbox',
-            pack: 'start',
-            align: 'stretch'
-        },
-        items: [{
-            xtype: 'textfield',
-            margin: '10 0',
-            label: 'Login',
-            bind: '{login}',
-            fieldLabel: 'Login',
-            reference: 'regLogField',
-            scrollable: true,
-            border: true
-        }, {
-            xtype: 'textfield',
-            margin: '10 0',
-            label: 'Password',
-            inputType: 'password',
-            bind: '{password}',
-            fieldLabel: 'Password',
-            reference: 'regPassField',
-            scrollable: true,
-            border: true
-        },{
-            xtype: 'textfield',
-            margin: '10 0',
-            label: 'Password',
-            inputType: 'password',
-            bind: '{repeatPassword}',
-            fieldLabel: 'Repeat password',
-            reference: 'regPassRepField',
-            scrollable: true,
-            border: true
-        }, {
-            xtype: 'fieldcontainer',
-            margin: '10 0',
-            fieldLabel: '',
-            defaultType: 'radiofield',
-            defaults: {
-                flex: 1
+    bodyPadding: '10',
+    items: [
+        {
+            xtype: 'form',
+            bodyPadding: 10,
+            scrollable:true,
+            maxWidth: 360,
+            fieldDefaults: {
+                labelAlign: 'right',
+                msgTarget: 'side'
             },
-            layout: 'column',
-            items: [
+
+            items: [{
+                xtype: 'fieldset',
+                title: 'Register',
+                defaultType: 'textfield',
+                defaults: {
+                    anchor: '100%'
+                },
+
+                items: [
+                    {
+                        allowBlank:false,
+                        bind: '{login}',
+                        fieldLabel: 'Login',
+                        reference: 'regLogField',
+                        vtype: 'alphanum',
+                        emptyText: 'login' },
+                    {
+                        allowBlank:false,
+                        inputType: 'password',
+                        bind: '{password}',
+                        fieldLabel: 'Password',
+                        reference: 'regPassField',
+                        vtype: 'alphanum',
+                        emptyText: 'password'},
+                    {
+                        allowBlank:false,
+                        fieldLabel: 'Verify',
+                        bind: '{repeatPassword}',
+                        reference: 'regPassRepField',
+                        emptyText: 'password',
+                        vtype: 'alphanum',
+                        inputType: 'password' }
+                ]
+            },{
+                xtype: 'fieldcontainer',
+                        margin: '10',
+                        fieldLabel: '',
+                        defaultType: 'radiofield',
+                        defaults: {
+                            flex: 1
+                        },
+                        layout: 'column',
+                        items: [
+                            {
+                                boxLabel: 'User',
+                                margin: '0 50',
+                                name: 'user',
+                                id: 'radioUserButton',
+                                bind: '{radioUser}'
+                            }, {
+                                boxLabel: 'Service',
+                                name: 'user',
+                                id: 'radioServiceButton',
+                                bind: 'radioService'
+                            }]
+            },
                 {
-                    boxLabel: 'User',
-                    margin: '0 50 0 50',
-                    name: 'user',
-                    id: 'radioUserButton',
-                    bind: '{radioUser}'
+                xtype: 'fieldset',
+                title: 'Info',
+                defaultType: 'textfield',
+                defaults: {
+                    anchor: '100%'
+                },
+
+                items: [{
+                    bind: {
+                        fieldLabel: '{radioUser ? "User name" : "Service name"}',
+                        emptyText: '{radioUser ? "User name" : "Service name"}'
+                    },
+                    vtype: 'alpha',
+                    reference: 'regFieldOne'
                 }, {
-                    boxLabel: 'Service',
-                    name: 'user',
-                    id: 'radioServiceButton',
-                    bind: 'radioService'
+                    bind: {
+                        fieldLabel: '{radioUser ? "Car brand" : "Location"}',
+                        emptyText: '{radioUser ? "Car brand" : "Location"}'
+                    },
+                    vtype: 'alphanum',
+                    reference: 'regFieldTwo'
+                }, {
+                    bind: {
+                        fieldLabel: '{radioUser ? "Tire radius" : "Serving staff"}',
+                        emptyText: '{radioUser ? "Tire radius" : "Serving staff"}',
+                    },
+                    vtype:'passNumber',
+                    reference: 'regFieldThree'
+                }, {
+                    bind: {
+                        fieldLabel: '{radioUser ? "Tire type" : "Rating"}',
+                        emptyText: '{radioUser ? "Tire type" : "Rating"}',
+                        hidden: '{!radioUser}'
+                    },
+                    reference: 'regFieldFour'
                 }]
-        }, {
-            xtype: 'textfield',
-            margin: '10 0',
-            bind: {
-                fieldLabel: '{radioUser ? "User name" : "Service name"}'
-            },
-            reference: 'regFieldOne'
-        }, {
-            xtype: 'textfield',
-            margin: '10 0',
-            bind: {
-                fieldLabel: '{radioUser ? "Car brand" : "Location"}'
-            },
-            reference: 'regFieldTwo'
-        }, {
-            xtype: 'textfield',
-            margin: '10 0',
-            bind: {
-                fieldLabel: '{radioUser ? "Tire radius" : "Serving staff"}'
-            },
-            reference: 'regFieldThree'
-        }, {
-            xtype: 'textfield',
-            margin: '10 0',
-            bind: {
-                fieldLabel: '{radioUser ? "Tire type" : "Rating"}',
-            },
-            reference: 'regFieldFour',
-        },
-            {
-                xtype: 'button',
-                scale: 'medium',
-                align: 'stretch',
-                margin: '10 0',
-                handler: 'onRegistration',
+            }],
+
+            buttons: [{
                 bind: {
                     disabled: '{!password||!login||!repeatPassword||(password!=repeatPassword)}',
                     text: 'Register'
-                }
-            },
-            {
-                xtype: 'button',
-                margin: '10 0',
+                },
                 scale: 'medium',
+                width: 160,
+                handler: 'onRegistration',
+                disabled: true
+            }, {
+                scale: 'medium',
+                width: 150,
                 handler: 'onLoginClick',
                 text: 'Login'
-            }
-        ]}]
+                }]
+        }
+    ]
+
+
+    // layout: 'center',
+    // bodyPadding: '50',
+    // items: [{
+    //     xtype: 'panel',
+    //     bodyPadding: '10 10',
+    //     title: 'Registration form',
+    //     scrollable: true,
+    //     width: 300,
+    //     layout: {
+    //         type: 'vbox',
+    //         pack: 'start',
+    //         align: 'stretch'
+    //     },
+    //     items: [{
+    //         xtype: 'textfield',
+    //         margin: '10 0',
+    //         label: 'Login',
+    //         bind: '{login}',
+    //         fieldLabel: 'Login',
+    //         reference: 'regLogField',
+    //         scrollable: true,
+    //         border: true
+    //     }, {
+    //         xtype: 'textfield',
+    //         margin: '10 0',
+    //         label: 'Password',
+    //         inputType: 'password',
+    //         bind: '{password}',
+    //         fieldLabel: 'Password',
+    //         reference: 'regPassField',
+    //         scrollable: true,
+    //         border: true
+    //     },{
+    //         xtype: 'textfield',
+    //         margin: '10 0',
+    //         label: 'Password',
+    //         inputType: 'password',
+    //         bind: '{repeatPassword}',
+    //         fieldLabel: 'Repeat password',
+    //         reference: 'regPassRepField',
+    //         scrollable: true,
+    //         border: true
+    //     }, {
+    //         xtype: 'fieldcontainer',
+    //         margin: '10 0',
+    //         fieldLabel: '',
+    //         defaultType: 'radiofield',
+    //         defaults: {
+    //             flex: 1
+    //         },
+    //         layout: 'column',
+    //         items: [
+    //             {
+    //                 boxLabel: 'User',
+    //                 margin: '0 50 0 50',
+    //                 name: 'user',
+    //                 id: 'radioUserButton',
+    //                 bind: '{radioUser}'
+    //             }, {
+    //                 boxLabel: 'Service',
+    //                 name: 'user',
+    //                 id: 'radioServiceButton',
+    //                 bind: 'radioService'
+    //             }]
+    //     }, {
+    //         xtype: 'textfield',
+    //         margin: '10 0',
+    //         bind: {
+    //             fieldLabel: '{radioUser ? "User name" : "Service name"}'
+    //         },
+    //         reference: 'regFieldOne'
+    //     }, {
+    //         xtype: 'textfield',
+    //         margin: '10 0',
+    //         bind: {
+    //             fieldLabel: '{radioUser ? "Car brand" : "Location"}'
+    //         },
+    //         reference: 'regFieldTwo'
+    //     }, {
+    //         xtype: 'textfield',
+    //         margin: '10 0',
+    //         bind: {
+    //             fieldLabel: '{radioUser ? "Tire radius" : "Serving staff"}'
+    //         },
+    //         reference: 'regFieldThree'
+    //     }, {
+    //         xtype: 'textfield',
+    //         margin: '10 0',
+    //         bind: {
+    //             fieldLabel: '{radioUser ? "Tire type" : "Rating"}',
+    //         },
+    //         reference: 'regFieldFour',
+    //     },
+    //         {
+    //             xtype: 'button',
+    //             scale: 'medium',
+    //             align: 'stretch',
+    //             margin: '10 0',
+    //             handler: 'onRegistration',
+    //
+    //         },
+    //         {
+    //             xtype: 'button',
+    //             margin: '10 0',
+    //
+    //         }
+    //     ]}]
 });
